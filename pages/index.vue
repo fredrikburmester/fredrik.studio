376de11df2403f8e0f8b466f22f988a430ca654e
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-03-23 09:03:15 +0100

    remove initial album fetch bug

--- pages/index.vue
+++ pages/index.vue
@@ -11,22 +11,22 @@ import * as firebase from 'firebase/app'
 export default {
     name: 'Index',
     created() {
-        this.getFromFirestore()
+        // this.getFromFirestore()
         $nuxt.$router.push("/Home")
     },
     mounted: (to, from, next) => {
         // $nuxt.$router.push("/Home")
     },
     methods: {
-        async getFromFirestore() {
-            var albums = []
-            const snapshot = await collection(db, "album")
-            const dd = await getDocs(snapshot)
-            dd.forEach(d => {
-                albums.push(d.id)
-            });
-            this.$store.commit("setAlbums", albums)
-        },
+        // async getFromFirestore() {
+        //     var albums = []
+        //     const snapshot = await collection(db, "album")
+        //     const dd = await getDocs(snapshot)
+        //     dd.forEach(d => {
+        //         albums.push(d.id)
+        //     });
+        //     this.$store.commit("setAlbums", albums)
+        // },
     }
 }
 </script>
